cce9308aa0dbc6a3ee54fabb402ffb3aa0ca373d
Juan VIlla <juan.es-19@hotmail.com>
2020-07-04 18:06:10 -0500

    Validations added using class-validator package

--- src/modules/user/register/RegisterInput.ts
+++ src/modules/user/register/RegisterInput.ts
@@ -1,14 +1,17 @@
-import { MaxLength, length, isEmail } from "class-validator";
+import { Length, IsEmail } from "class-validator";
 import { InputType, Field } from "type-graphql";
 
 @InputType()
 export class RegisterInput {
+  @Length(1, 200)
   @Field()
   firstName!: string;
 
+  @Length(1, 200)
   @Field()
   lastName!: string;
 
+  @IsEmail()
   @Field()
   email!: string;
 
