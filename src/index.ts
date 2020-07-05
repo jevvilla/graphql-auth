072e85d486957d548bf0a0426ee91f180508c0e4
Juan VIlla <juan.es-19@hotmail.com>
2020-07-05 18:37:10 -0500

    middlewares implemented to be able to use RedisStore and express-session

--- src/index.ts
+++ src/index.ts
@@ -2,18 +2,52 @@ import "reflect-metadata";
 import { ApolloServer } from "apollo-server-express";
 import { buildSchema } from "type-graphql";
 import { createConnection } from "typeorm";
+import session from "express-session";
 import express from "express";
+import connectRedis from "connect-redis";
+import cors from "cors";
 
 import { RegisterResolver } from "./modules/user/register/RegisterResolver";
+import { LoginResolver } from "./modules/user/login/LoginResolver";
+import { redis } from "./redis";
 
 const main = async () => {
   await createConnection();
-  const app = express();
+
   const server = new ApolloServer({
     schema: await buildSchema({
-      resolvers: [RegisterResolver],
+      resolvers: [RegisterResolver, LoginResolver],
     }),
+    context: ({ req }) => ({ req }),
   });
+
+  const app = express();
+
+  const RedisStore = connectRedis(session);
+
+  app.use(
+    cors({
+      credentials: true,
+      origin: "http://localhost:3000",
+    })
+  );
+
+  app.use(
+    session({
+      store: new RedisStore({
+        client: redis as any,
+      }),
+      name: "gqlidt",
+      secret: "DugkSsgE487sS6j8GNmw",
+      resave: false,
+      saveUninitialized: false,
+      cookie: {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === "production",
+        maxAge: 1000 * 60 * 60 * 24 * 7,
+      },
+    })
+  );
 
   server.applyMiddleware({ app, path: "/graphql" });
   app.listen(4040, () =>
