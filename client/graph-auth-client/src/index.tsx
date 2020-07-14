9722585cb5c21b76a276f751fd433474c5b3cc15
Juan VIlla <juan.es-19@hotmail.com>
2020-07-14 10:51:43 -0500

    ApolloBoostClient set and ApolloProvider implemented

--- client/graph-auth-client/src/index.tsx
+++ client/graph-auth-client/src/index.tsx
@@ -1,14 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import ApolloBoostClient from "apollo-boost";
+import { ApolloProvider } from "@apollo/react-hooks";
+
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+
+const client = new ApolloBoostClient({
+  uri: "http://localhost:4040/graphql",
+  credentials: "include",
+});
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
